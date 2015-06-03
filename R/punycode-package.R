c6dea3264e3b7e4ec78c0f722f84f6a7ed23b1d2
Bob Rudis <bob@rudis.net>
2015-06-03 13:55:15 -0400

    better pkg description

--- R/punycode-package.R
+++ R/punycode-package.R
@@ -1,4 +1,8 @@
-#' A package to handle punycode domains (encoding /)
+#' A package to handle punycode domains (encoding / decoding)
+#'
+#' It uses the GNU libidn \url{http://www.gnu.org/software/libidn/} library and
+#' provides functions to encode & decode international domain names.
+#'
 #' @name punycode
 #' @docType package
 #' @author Bob Rudis (@@hrbrmstr)
