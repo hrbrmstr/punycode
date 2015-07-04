3ca54224e402c3917dc7d23fcd15b7d85bf755f7
Bob Rudis <bob@rudis.net>
2015-07-04 17:43:46 -0400

    config/test

--- tests/testthat/test-punycode.R
+++ tests/testthat/test-punycode.R
@@ -1,7 +1,8 @@
 context("encoding")
 test_that("we can encode domains", {
 
-  expect_that(puny_encode("xn------qpeiobbci9acacaca2c8a6ie7b9agmy.net"), matches("זגורי-אימפריה-לצפייה-ישירה.n"))
+  expect_that(puny_encode("xn------qpeiobbci9acacaca2c8a6ie7b9agmy.net"),
+              matches("זגורי-אימפריה-לצפייה-ישירה.n"))
   expect_that(puny_encode("rudis.net"), matches("rudis.net"))
 
 })
@@ -9,8 +10,10 @@ test_that("we can encode domains", {
 context("decoding")
 test_that("we can decode domains", {
 
-  expect_that(puny_decode("도매cook.com"), matches("xn--cook-9k1q670a.com"))
-  expect_that(puny_decode("rudis.net"), matches("rudis.net"))
+  expect_that(puny_decode("도매cook.com"),
+              matches("xn--cook-9k1q670a.com"))
+  expect_that(puny_decode("rudis.net"),
+              matches("rudis.net"))
 
 })
 
